3f45eb5e2eb9d6b3356931757e405a8ac841d71e
Daryu <166614442+LuquyPest@users.noreply.github.com>
2026-04-29 22:59:11 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http",  hostname: "**" },
+    ],
+  },
+}
+module.exports = nextConfig
